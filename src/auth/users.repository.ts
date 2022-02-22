321d49152dd3ea99764a0b4542a39e808b8bf4df
Riley <increaseblue666@gmail.com>
2022-02-22 21:52:34 +0900

    Chore: 사용하지 않는 import 삭제

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,7 +1,4 @@
-import {
-  ConflictException,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { ConflictException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
