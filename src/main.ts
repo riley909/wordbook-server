d116b60513166749d826791c194ee6b2a25fa7af
Riley <increaseblue666@gmail.com>
2022-02-23 16:18:47 +0900

    Modify: 포트 변경

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3000);
-  logger.log(`🚀 Application listening on port 3000`);
+  await app.listen(4000);
+  logger.log(`🚀 Application listening on port 4000`);
 }
 bootstrap();
